68e4ba56c0476d3efa2017ce7518150b98c32503
carsonratcliff <carso910@hotmail.com>
2017-12-17 13:52:27 -0500

    Implement basic login

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,12 +3,14 @@ require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
    
    test "layout links" do
+    # 5.32
      get root_path
      assert_template 'static_pages/home'
      assert_select "a[href=?]", root_path, count: 2
      assert_select "a[href=?]", help_path
      assert_select "a[href=?]", about_path
      assert_select "a[href=?]", contact_path
+     # 5.36
      get contact_path
      assert_select "title", full_title("Contact")
    end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,7 +8,8 @@ require 'rails/test_help'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  
+  include ApplicationHelper # 5.35
+
   # Returns true if a test user is logged in.
   def is_logged_in?
     !session[:user_id].nil?
